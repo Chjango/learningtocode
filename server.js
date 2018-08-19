bbada40aeb85aec5c52453632ce2a36a6d0f15e1
Chjango <chjangounchained@gmail.com>
2018-08-18 21:34:15 -0700

    add verify hash

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const crypto = require('crypto');
+const db = new Map();
 
 function hashPass(password) {
   const hash = crypto.createHash('sha256');
@@ -8,8 +9,30 @@ function hashPass(password) {
   return hash.digest('hex');
 }
 
+function createUser(username, password) {
+  if (db.has(username)) {
+    throw new Error('Username exists');
+  }
+  const hash = hashPass(password);
+  db.set(username, hash);
+}
+
+function verifyUser(username, password) {
+  if (!db.has(username)) {
+    return false;
+  }
+  const hash1 = db.get(username);
+  const hash2 = hashPass(password);
+  if (hash1 === hash2) {
+    return true;
+  }
+  return false;
+}
 //                  0           1               2
 // process.argv = ['node', 'server.js', 'hello world'];
-const arg = process.argv[2];
-const hash = hashPass(arg);
-console.log(hash);
+// const arg = process.argv[2];
+// const hash = hashPass(arg);
+// console.log(hash);
+
+createUser('Chjango', 'foobar');
+console.log(verifyUser('Chjango', 'foobar2'));
